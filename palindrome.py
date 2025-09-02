9b2357dcdb03b34ba8de0fb4dc5d691caae5303d
kollibrunda <22brunda@gmail.com>
2025-09-02 20:24:56 +0530

    Add files via upload

---
+++ palindrome.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if x<0:
+            return False
+        x_str = str(x)
+        if x_str == x_str[::-1]:
+            return True
+        else:
+            return False
+x = int(input("enter your number"))
+s = Solution()
+k = s.isPalindrome(x)
+print(k)
