59c011fe17c101119d14a23e907b764e900e63f3
kollibrunda <22brunda@gmail.com>
2026-07-10 20:37:27 +0530

    Add files via upload

---
+++ count and say.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def countAndSay(self, n):
+        """
+        :type n: int
+        :rtype: str
+         """
+        if n==1:
+            return "1"
+        prev = "1"
+        for _ in range(2,n+1):
+            res=""
+            c=1
+            for i in range(1,len(prev)):
+                if prev[i]==prev[i-1]:
+                    c+=1
+                else:
+                    res += str(c)+prev[i-1]
+                    c=1
+            res+=str(c)+prev[-1]
+            prev=res
+        return prev 
+n=int(input())
+print(Solution().countAndSay(n))
